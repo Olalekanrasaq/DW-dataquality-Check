4534565f1624351c27fc53aeaf29c64af853fb25
Olalekan Rasaq <69814366+Olalekanrasaq@users.noreply.github.com>
2025-04-21 10:27:35 +0100

    Add files via upload

---
+++ dbconnect.py
@@ -0,0 +1,20 @@
+import os
+import psycopg2
+pgpassword = "Analyst@100"
+conn = None
+try:
+    conn = psycopg2.connect(
+        user = "olalekanrasaq1331",
+        password = pgpassword,
+        host = "pgdb",
+        port = "5432",
+        database = "billingDW")
+except Exception as e:
+    print("Error connecting to data warehouse")
+    print(e)
+else:
+    print("Successfully connected to warehouse")
+finally:
+    if conn:
+        conn.close()
+        print("Connection closed")
